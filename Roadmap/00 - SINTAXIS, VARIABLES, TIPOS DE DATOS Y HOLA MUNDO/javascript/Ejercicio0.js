13dc1ea0ba425a58756746f050e8d5064b463dac
Angie N. Gonzalez <nthalygc1997@gmail.com>
2024-08-02 14:48:39 -0500

    Create Ejercicio0.js

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/Ejercicio0.js
@@ -0,0 +1,20 @@
+// https://developer.mozilla.org/es/docs/Web/JavaScript
+
+/* Este es un comentario
+multiple linea mi lenguaje de programacion de Java script*/
+
+let name = "Angie";
+
+let numero = 1;
+let decimal = 2.5;
+let cadena = "Sogia";
+let booleano = true;
+let indefinido = undefined;
+
+console.log("Hola, JavaScript");
+
+console.log(typeof numero)
+console.log(typeof cadena)
+console.log(typeof booleano)
+console.log(typeof indefinido)
+console.log(typeof decimal)
